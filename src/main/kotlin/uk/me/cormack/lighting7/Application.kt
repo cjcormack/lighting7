3c4f6b8c96bb77355f897a97e4f38d241465a27d
Chris Cormack <cjc@photoshelter.com>
2023-11-21 20:41:50 +0000

    Tidy up automatic setting of config args.

--- src/main/kotlin/uk/me/cormack/lighting7/Application.kt
+++ src/main/kotlin/uk/me/cormack/lighting7/Application.kt
@@ -5,20 +5,19 @@ import io.ktor.server.netty.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.launch
-import org.jetbrains.exposed.sql.transactions.transaction
 import uk.me.cormack.lighting7.dao.DatabaseFactory
-import uk.me.cormack.lighting7.models.Script
-import uk.me.cormack.lighting7.models.Scripts
 import uk.me.cormack.lighting7.plugins.*
 import uk.me.cormack.lighting7.show.Show
 
-fun main(args: Array<String>): Unit {
-    val argsWithDefaults = if (args.any { it.startsWith("-config=") }) {
-        args
-    } else {
-        args + "-config=application.conf" + "-config=local.conf"
+fun main(argv: Array<String>) {
+    val args = mutableListOf("-config=application.conf")
+    args += argv.filter { it != "-config=application.conf" }
+
+    if (args.none { it.startsWith("-config=") && it != "-config=application.conf" }) {
+        args += "-config=local.conf"
     }
-    EngineMain.main(argsWithDefaults)
+
+    EngineMain.main(args.toTypedArray())
 }
 
 @OptIn(ExperimentalCoroutinesApi::class, ObsoleteCoroutinesApi::class)
